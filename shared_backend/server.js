ed5a452434b4410a62401aa354a845d3934a963d
Denialenial <motebang763@gmail.com>
2024-12-06 06:45:36 -1200

    Update server.js to use a different port

--- shared_backend/server.js
+++ shared_backend/server.js
@@ -31,7 +31,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
